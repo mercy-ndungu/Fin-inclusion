251b5bd0f74489ec5ac36a5a5d2845b58371f01e
Kirigo Ndungu <mercyndungukirigo@gmail.com>
2023-08-22 16:55:11 +0300

    Ammended components and added pages to my react architecture

---
+++ kolafront/src/pages/Home/home.js
@@ -0,0 +1,16 @@
+//import Navbar from "../components/Navbar/navbar";
+import Navbar from "../components/navbar/navbar";
+import "./navbar.css"
+
+const Home = () => {
+    return (
+      <div>
+        <Navbar />
+      
+          
+
+      </div>
+    );
+  };
+  
+  export default Home;
