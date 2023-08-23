70891b086afc75df434041683107d64e4272aca3
Kirigo Ndungu <mercyndungukirigo@gmail.com>
2023-08-23 16:05:28 +0300

    Modified navbar

--- kolafront/src/pages/Home/home.js
+++ kolafront/src/pages/Home/home.js
@@ -1,6 +1,5 @@
 //import Navbar from "../components/Navbar/navbar";
-import Navbar from "../components/navbar/navbar";
-import "./navbar.css"
+import Navbar from "../../components/Navbar/navbar";
 
 const Home = () => {
     return (
