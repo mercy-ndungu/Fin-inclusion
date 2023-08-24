6d10de1c6c3610d61d199d0516393d8f44ec723d
Kirigo Ndungu <mercyndungukirigo@gmail.com>
2023-08-24 16:33:44 +0300

    Modified home page

--- kolafront/src/pages/Home/home.js
+++ kolafront/src/pages/Home/home.js
@@ -1,5 +1,7 @@
 //import Navbar from "../components/Navbar/navbar";
 import Navbar from "../../components/Navbar/navbar";
+import {SvgBlob} from 'react-svg-blob';
+// import {cross as crossPattern} from 'react-svg-blob/dist/lib/patterns';
 
 const Home = () => {
     return (
@@ -7,7 +9,12 @@ const Home = () => {
         
         <Navbar />
       
-          
+        
+<SvgBlob variant='solid' color='#00cec9' />;
+
+<SvgBlob variant='gradient' colors={['#2980B9', '#6DD5FA']} />
+
+{/* <SvgBlob variant='pattern' pattern={crossPattern} color='#d1d8e0' isOutline /> */}
 
       </div>
     );
